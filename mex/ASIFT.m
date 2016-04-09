0701116d437788cc23cfbec60c3bb0207268232e
BjwCode <532433419@qq.com>
2016-04-09 10:53:32 +0800

    C++

--- mex/ASIFT.m
+++
@@ -1,4 +0,0 @@
-function [f1, f2, d1, d2] = ASIFT(img1, img2, numTilts, resize)
-    [f1,d1] = mexASIFT(img1', numTilts, resize);
-    [f2,d2] = mexASIFT(img2', numTilts, resize);
-end
--- mex/cv_match.m
+++
@@ -1,6 +0,0 @@
-function [matches, matches_all] = cv_match( d1, d2, sift_thres, flag_flann )
-%CPU_MATCH Summary of this function goes here
-%   Detailed explanation goes here
-    [matches, matches_all] = mexMatching(d1, d2, sift_thres, flag_flann);
-end
-
